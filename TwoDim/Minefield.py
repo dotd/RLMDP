34ba062e2624a8818686c4a97bbb1428057f8589
joren <joren@yahoo-inc.com>
2018-08-15 11:20:21 +0300

    Bugfixes

--- TwoDim/Minefield.py
+++ TwoDim/Minefield.py
@@ -15,7 +15,7 @@ class Minefield(Env):
                  step_cost=1,
                  rand_action_prob=0.05,
                  num_mines=80,
-                 start=np.array([np.array([60, 53])]),
+                 start=np.array([np.array([60, 52])]),
                  terminal_states=np.array([np.array([60, 2])]),
                  random_generator=None):
 
@@ -26,7 +26,7 @@ class Minefield(Env):
         self.num_mines = num_mines
         # All possible coordinates+minefield tuples
         self.observation_space = [np.array(coord) for coord in product(*[range(d) for d in shape])]
-        self.minefield = self.gen_field(num_mines=num_mines)
+
         self.action_space = list(np.vstack([np.eye(self.dim), -1 * np.eye(self.dim)]))
         self.mine_penalty = mine_penalty
         self.reach_reward = reach_reward
@@ -35,6 +35,7 @@ class Minefield(Env):
         self.start_states = start
         self.terminal_states = terminal_states
         self.cur_state = self.get_random_start_state()
+        self.minefield = self.gen_field(num_mines=num_mines)
         # Need to generate the mine field here below. Figure it out later.
 
     def _step(self, action):
@@ -56,7 +57,7 @@ class Minefield(Env):
     def _reset(self):
         self.cur_state = self.get_random_start_state()
 
-    def _render(self, mode='human'):
+    def _render(self, mode='human', close=False):
         return self.minefield
 
     def _seed(self, seed=42):
@@ -86,21 +87,23 @@ class Minefield(Env):
         Generates a random Minefield
         :return:
         """
-        minefield = np.zeros(self.shape)
+        self.minefield = np.zeros(self.shape)
         # Mines cannot be located in a start state or a terminal state
         candidate_locations = [coord for coord in self.observation_space
                                if coord not in self.terminal_states + self.start_states]
-        for coord in self.rand_gen.choice(range(len(candidate_locations)), num_mines, replace=False):
-            minefield[self.observation_space[coord]] = 1
-        return minefield
+        mine_indices = self.rand_gen.choice(range(len(candidate_locations)), num_mines, replace=False)
+        for coord in mine_indices:
+            self.minefield[tuple(candidate_locations[coord])] = 1
+        return self.minefield
 
-    def compute_next_state(self, state, action):
+    def compute_next_state(self, action, state=None):
         """
         Return the next step, after applying the action. Corrects for boundary limitations
         :param state: Numpy array of coordinates
         :param action:
         :return: a new state, respecting the borders of the minefield
         """
+        state=self.cur_state if state==None else state
         return np.minimum(np.maximum(state + action,
                           np.zeros(len(self.shape))),
                           self.shape - 1)
@@ -112,3 +115,8 @@ class Minefield(Env):
         :return:
         """
         return np.unique([self.compute_next_state(state, action) for action in self.action_space], axis=0)
+
+if __name__ == "__main__":
+    m = Minefield()
+    actions = m.action_space
+    print(actions)
